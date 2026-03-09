59baef9983174eea678a9fb4272538f85660cecc
agudelon496-ctrl <agudelon496@gmail.com>
2026-03-09 12:47:40 +0000

    login

---
+++ config/conexion.php
@@ -0,0 +1,31 @@
+<?php
+
+class Conexion {
+
+    private $host = "localhost";
+    private $db_name = "hospital_pro";
+    private $username = "root";
+    private $password = "";
+    private $conn;
+
+    public function getConnection(){
+
+        $this->conn = null;
+
+        try{
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        }catch(PDOException $exception){
+            echo "Error de conexión: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
---
+++ vistas/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Login MedicarFlow</title>
+
+</head>
+
+<body>
+
+<h2>Ingreso al Sistema</h2>
+
+<form action="../controladores/login_control.php" method="POST">
+
+<input type="text" name="usuario" placeholder="Usuario">
+
+<br><br>
+
+<input type="password" name="password" placeholder="Contraseña">
+
+<br><br>
+
+<button type="submit">Ingresar</button>
+
+</form>
+
+</body>
+</html>
